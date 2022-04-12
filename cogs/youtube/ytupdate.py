9f1a01042281da6dd11b6a4106fcdcb21d1438ef
Cullen <cullen.hutchison@gmail.com>
2022-04-12 11:24:35 -0500

    Added

--- cogs/youtube/ytupdate.py
+++ cogs/youtube/ytupdate.py
@@ -12,6 +12,7 @@ class YtUpdate(commands.Cog):
 
     @commands.command(aliases=['setchannel', 'SetChannel'])
     @commands.has_permissions(administrator=True)
+    async def set_channel(self, ctx, channel_url, *, upload_message):
     async def set_channel(self, ctx, channel_url_extension, *, upload_message=None):
         """
         Sets the current text channel up for notifications for a given youtube channel. 
